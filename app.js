886f858c8239d62604d4dd65aeed471761c2a7a2
Jeremy Fowler <jembyfowler@gmail.com>
2025-02-18 11:43:31 -0700

    added buttons and array of data

---
+++ app.js
@@ -0,0 +1,34 @@
+// SECTION global variables 🧠
+
+const menuItems = [
+  {
+    name: 'spaghetti',
+    price: 7,
+    quantity: 0
+  },
+  {
+    name: 'fettuccine',
+    price: 30.99,
+    quantity: 0
+  },
+  {
+    name: 'chips and salsa',
+    price: 1.99,
+    quantity: 0
+  },
+  {
+    name: 'quesadilla',
+    price: 15.99,
+    quantity: 0
+  },
+]
+
+// !SECTION
+
+// SECTION actions / logic ⚙️
+
+// !SECTION
+
+// SECTION drawing (DOM manipulation) ✏️
+
+// !SECTION
